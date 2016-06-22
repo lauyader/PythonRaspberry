f1deb0949f42c620c59a82b51193d1acf798fc14
Luis Americo Auyadermont Muñoz <lauyader2011@gmail.com>
2016-06-21 21:42:14 -0430

    Hola Mundo, encendido de led con python raspberry

---
+++ led.py
@@ -0,0 +1,11 @@
+#!usr/bin/env/ python
+#enciende.py
+#importamos la libreria GPIO
+import RPi.GPIO as GPIO 
+#Definimos el modo BCM 
+GPIO.setmode(GPIO.BCM) 
+#Ahora definimos el pin GPIO 17 como salida
+GPIO.setup(12, GPIO.OUT) 
+#Y le damos un valor logico alto para encender el LED
+GPIO.output(12, GPIO.HIGH) 
+
